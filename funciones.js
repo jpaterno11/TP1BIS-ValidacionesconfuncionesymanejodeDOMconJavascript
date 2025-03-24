7ba5444698b8b8364c61af0db5563a1e87a273e0
jpaterno <joaquin1803paterno@gmail.com.com>
2025-03-24 15:15:11 -0300

    terminado

--- funciones.js
+++ funciones.js
@@ -10,7 +10,7 @@ function validarForm()
     }
     else{
         calcularPromedio();
-        
+        event.preventDefault();
     }
 }
 function mostrarMensaje(id) {
@@ -21,27 +21,33 @@ function esconderMensaje(id) {
 }
 
 function confirmarNota(nota, mensaje){
-    const nota1 = parseFloat(document.getElementById(nota)); 
+    const nota1 = parseFloat(document.getElementById(nota).value);
     let mensajE=document.getElementById(mensaje);
     mensajE.style.color = (nota1 > 0 && nota1 < 11 ? "green" : "red");
-    mensajE.textContent = (nota1.value.length > 2 ? "El nombre contiene al menos 3 caracteres" : "El nombre no contiene al menos 3 caracteres");
+    mensajE.textContent = (nota1 > 0 && nota1 < 11 ? "La nota es mayor a 0 y menor a 11" : "La nota no es o mayor a 0 o menor a 10");
     return (mensajE.style.color == "green");
 }
 
 function calcularPromedio(){
-    const nota1 = parseFloat(document.getElementById('notaMatematica')); 
-    const nota2 = parseFloat(document.getElementById('notaLengua')); 
-    const nota3 = parseFloat(document.getElementById('notaEFSI'));
+    const nota1 = parseFloat(document.getElementById('notaMatematica').value); 
+    const nota2 = parseFloat(document.getElementById('notaLengua').value); 
+    const nota3 = parseFloat(document.getElementById('notaEFSI').value);
     let mostrarResultado =document.getElementById('Resultado'); 
-    let materiaAlta =document.getElementById('MateriaAlta'); 
     let promedio = ((nota1 + nota2 + nota3) / 3)
     mostrarResultado.textContent = "El promedio es: " + promedio;
     mostrarResultado.style.color = (promedio >= 6 ? "green" : "red");
+}
+function mostrarMasAlta() {
+    const nota1 = parseFloat(document.getElementById('notaMatematica').value); 
+    const nota2 = parseFloat(document.getElementById('notaLengua').value); 
+    const nota3 = parseFloat(document.getElementById('notaEFSI').value);
+    let materiaAlta =document.getElementById('MateriaAlta'); 
     let materiasAltas = [];
     let maxNota = Math.max(nota1, nota2, nota3);
     if (nota1 === maxNota) materiasAltas.push("Matematica");
     if (nota2 === maxNota) materiasAltas.push("Lengua");
     if (nota3 === maxNota) materiasAltas.push("EFSI");
+    materiaAlta.style.color = "blue";
     if (materiasAltas.length === 1) {
         materiaAlta.textContent = "La materia más alta es: " + materiasAltas[0];
     } else {
